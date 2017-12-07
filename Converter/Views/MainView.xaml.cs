023585e9ed9458f86c0136a24ce0e76a57c601fa
François <flevesque85@gmail.com>
2017-12-07 09:09:43 -0500

    Added folders for MVVM project

---
+++ Converter/Views/MainView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Autodesk.AutoCAD.EditorInput;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Converter.Views
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
